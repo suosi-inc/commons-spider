9eb912f2bd94bd3c154c98fe4e934e41a826677e
niuchaoqun <smallmenu@gmail.com>
2019-02-21 11:46:47 +0800

    * update

---
+++ src/main/java/com/github/suosi/commons/spider/App.java
@@ -0,0 +1,7 @@
+package com.github.suosi.commons.spider;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/github/suosi/commons/spider/AppTest.java
@@ -0,0 +1,12 @@
+package com.github.suosi.commons.spider;
+
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+
+}
