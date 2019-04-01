cdcf1a13b427f725b17bd75bb1558b4ecb019578
niuchaoqun <niuchaoqun@coowin.co>
2019-04-02 00:31:42 +0800

    * SiteExtract

--- src/main/java/com/github/suosi/commons/spider/utils/UrlUtils.java
+++ src/main/java/com/github/suosi/commons/spider/utils/UrlUtils.java
@@ -9,6 +9,7 @@ public class UrlUtils {
     public static boolean verifyUrl(String url) {
         return StringUtils.isNotBlank(url)
                 && !StringUtils.startsWithIgnoreCase(url, "javascript:")
+                && !StringUtils.startsWithIgnoreCase(url, "mailto:")
                 && !StringUtils.startsWithIgnoreCase(url, "#");
     }
 }
