56a55b51265c59cca20129ae47c1da04c0f42b29
niuchaoqun <smallmenu@gmail.com>
2019-04-16 15:08:34 +0800

    * update

--- src/test/java/com/github/suosi/commons/spider/utils/OkHttpUtilsTest.java
+++ src/test/java/com/github/suosi/commons/spider/utils/OkHttpUtilsTest.java
@@ -1,12 +1,16 @@
 package com.github.suosi.commons.spider.utils;
 
-import okhttp3.Response;
+import okhttp3.*;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+import static com.github.suosi.commons.helper.Static.base64Encode;
 
 public class OkHttpUtilsTest {
 
@@ -33,6 +37,67 @@ public class OkHttpUtilsTest {
                     Document document = Jsoup.parse(html);
                     String title = StringUtils.trimToEmpty(document.title());
                     System.out.println(url + " " + StringUtils.upperCase(charset) + " " + title);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void proxyUserPassword() {
+        String url = "http://test.abuyun.com";
+
+        // 开启代理模式
+        Authenticator proxyAuthenticator = (route, r) -> {
+            String credential = Credentials.basic("HG686Y9765V56R8D", "491953B253B48306");
+            return r.request().newBuilder()
+                    .header("Proxy-Authorization", credential)
+                    .build();
+        };
+
+        OkHttpClient client = OkHttpUtils.builder()
+                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("http-dyn.abuyun.com", 9020)))
+                .proxyAuthenticator(proxyAuthenticator)
+                .build();
+
+        for (int i = 0; i < 3; i++) {
+            try (Response response = client.newCall(OkHttpUtils.request(url)).execute()) {
+                System.out.println(response.code());
+                if (response.isSuccessful() && response.body() != null) {
+                    byte[] bytes = response.body().bytes();
+                    String charset = CharsetUtils.guessCharset(bytes, response);
+                    String html = new String(bytes, charset);
+
+                    System.out.println(html);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    public void proxyHeader() {
+        String url = "http://test.abuyun.com";
+
+        OkHttpClient client = OkHttpUtils.builder()
+                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("http-dyn.abuyun.com", 9020)))
+                .build();
+
+        Request request = OkHttpUtils.requestBuilder(url)
+                .header("Proxy-Authorization", "Basic " + base64Encode("HG686Y9765V56R8D:491953B253B48306"))
+                .build();
+
+        for (int i = 0; i < 3; i++) {
+            try (Response response = client.newCall(request).execute()) {
+                System.out.println(response.code());
+                if (response.isSuccessful() && response.body() != null) {
+                    byte[] bytes = response.body().bytes();
+                    String charset = CharsetUtils.guessCharset(bytes, response);
+                    String html = new String(bytes, charset);
+
+                    System.out.println(html);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
