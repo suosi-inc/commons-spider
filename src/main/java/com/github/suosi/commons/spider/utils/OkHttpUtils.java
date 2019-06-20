7f25375f44c349393403d122594f6e0137b509a5
niuchaoqun <smallmenu@gmail.com>
2019-06-20 10:29:51 +0800

    + UserAgentUtils

--- src/main/java/com/github/suosi/commons/spider/utils/OkHttpUtils.java
+++ src/main/java/com/github/suosi/commons/spider/utils/OkHttpUtils.java
@@ -14,7 +14,7 @@ public class OkHttpUtils {
 
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
 
-    private static final long TIMEOUT = 5;
+    private static final long TIMEOUT = 2;
 
     public static OkHttpClient.Builder builder() {
        return builder(DEFAULT_CONNECTION_POOL);
