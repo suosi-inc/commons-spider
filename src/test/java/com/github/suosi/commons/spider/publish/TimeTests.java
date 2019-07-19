ffee011a13198c7c7f8d17c706ee7096c5c08911
tao.yang <yangt@suosi.com.cn>
2019-07-19 13:49:51 +0800

    0.31.0

--- src/test/java/com/github/suosi/commons/spider/publish/TimeTests.java
+++ src/test/java/com/github/suosi/commons/spider/publish/TimeTests.java
@@ -6,24 +6,21 @@ import com.github.suosi.commons.spider.utils.OkHttpUtils;
 import okhttp3.Response;
 import org.junit.Test;
 
-import java.util.regex.Pattern;
-
 public class TimeTests {
     @Test
     public void test() {
-        String url = "http://www.wanan.gov.cn/4697346/4719443.html";
+        String url = "http://www.cnjiyin.com/qiye/20181114_124.html";
         String time = getHtml(url);
         System.out.println(time);
     }
 
     @Test
     public void time() {
-        String html = "2019年6月24日";
-
-        String ymd ="20\\d{2}[-/年.](0[1-9]|1[0-2]|[1-9])[-/月.](0[1-9]|[1-2][0-9]|3[0-1]|[1-9])[日T]?\\s{0,2}(([0-1][0-9]|2[0-3]|[1-9])[:点时]([0-5][0-9]|[0-9])([:分]([0-5][0-9]|[0-9]))?)?";
-
-        Pattern pattern = Pattern.compile(ymd);
-        System.out.println(pattern.matcher(html).find());
+        String html = "<div class=\"entry-info\">\n" +
+                "\t\t\t\t<span>2018-11-14 12:23:38</span>\n" +
+                "\t\t\t\t<span class=\"dot\">•</span>\n" +
+                "\t\t\t  <span>来源：</span>\n" +
+                "\t\t\t</div>\n";
 
         System.out.println(Parse.parsePublishTime(html));
     }
