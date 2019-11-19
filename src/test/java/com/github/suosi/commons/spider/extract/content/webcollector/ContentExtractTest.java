d37484d8088ec6fa28d1889dfc5b1e9b368cf12d
niuchaoqun <smallmenu@gmail.com>
2019-11-19 10:21:36 +0800

    * 0.53.0-SNAPSHOT 修改最大深度为256，解决 stackoverflow 的问题

--- src/test/java/com/github/suosi/commons/spider/extract/content/webcollector/ContentExtractTest.java
+++ src/test/java/com/github/suosi/commons/spider/extract/content/webcollector/ContentExtractTest.java
@@ -74,18 +74,19 @@ public class ContentExtractTest {
                 String charset = CharsetUtils.guessCharset(bytes, response);
                 String html = new String(bytes, charset);
 
-                // for (int i = 0; i < 3000; i++) {
-                //     html += "<div class=ssss_'"+ i + "'>";
-                // }
-                // html += "123123";
-                // for (int i = 0; i < 3000; i++) {
-                //     html += "</div>";
-                // }
+                for (int i = 0; i < 3000; i++) {
+                    html += "<div class=ssss_'"+ i + "'>";
+                }
+                html += "123123";
+                for (int i = 0; i < 3000; i++) {
+                    html += "</div>";
+                }
 
                 News newsByHtml = ContentExtractor.getNewsByHtml(html);
 //                System.out.println(newsByHtml);
                 System.out.println(Parse.parsePublishTime(html));
                 System.out.println(newsByHtml.getTitle());
+                System.out.println(newsByHtml.getTime());
                 System.out.println(newsByHtml.getContent());
             }
         } catch (IOException e) {
