e720fbe2b6bbd82a323b809821c667830fd4b558
chengliang <chengl@suosi.com.cn>
2019-12-14 16:40:30 +0800

    * 0.57.6-SNAPSHOT 调整site.domain增加超时设置

--- src/main/java/com/github/suosi/commons/spider/extract/site/SiteExtract.java
+++ src/main/java/com/github/suosi/commons/spider/extract/site/SiteExtract.java
@@ -24,6 +24,8 @@ public class SiteExtract {
 
     private static final String HTTPS_PROTOCOL = "https";
 
+    private static final String NOT_WWW = "not_www";
+
     private static final String[] DOMAIN_FILTERS = {
             "user",
             "u",
@@ -75,7 +77,7 @@ public class SiteExtract {
      * @return
      */
     public static Site domain(String domain) throws Exception {
-        String[] protocols = {HTTP_PROTOCOL, HTTPS_PROTOCOL};
+        String[] protocols = {HTTP_PROTOCOL, HTTPS_PROTOCOL, NOT_WWW};
 
         if (StringUtils.isNotBlank(domain)) {
             String mainDomain = domain;
@@ -93,9 +95,15 @@ public class SiteExtract {
 
             for (String protocol : protocols) {
                 // 构造 URL
-                String url = protocol + "://" + mainDomain;
+                String url;
+                if ("not_www".equals(protocol)) {
+                    url = "http" + "://" + domain;
+                } else {
+                    url = protocol + "://" + mainDomain;
+                }
 
                 try (Response response = OkHttpUtils.client().newCall(OkHttpUtils.request(url)).execute()) {
+                    // System.out.println(response.code());
                     if (response.isSuccessful() && response.body() != null) {
 
                         // 编码
@@ -129,8 +137,84 @@ public class SiteExtract {
 
                     }
                 } catch (IOException e) {
-                    // System.out.println(e.getLocalizedMessage() + ":" + url);
-                    throw new Exception("site domain except: " + e.getLocalizedMessage() + ":" + url);
+                    System.out.println(e.getLocalizedMessage() + ":" + url);
+                    // throw new Exception("site domain except: " + e.getLocalizedMessage() + ":" + url);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 根据域名抽取站点关键信息
+     *
+     * @param domain 域名
+     * @return
+     */
+    public static Site domain(String domain, Long timeoutSecond) throws Exception {
+        String[] protocols = {HTTP_PROTOCOL, HTTPS_PROTOCOL, NOT_WWW};
+
+        if (StringUtils.isNotBlank(domain)) {
+            String mainDomain = domain;
+
+            // 是否是顶级域名，如果是顶级域名会进行 www 前缀主域名探测
+            boolean top = false;
+            String topDomain = DomainUtils.topDomain(domain);
+            if (domain.equals(topDomain)) {
+                top = true;
+            }
+
+            if (top && !StringUtils.startsWithIgnoreCase(domain, WWW_PREFIX)) {
+                mainDomain = WWW_PREFIX + domain;
+            }
+
+            for (String protocol : protocols) {
+                // 构造 URL
+                String url;
+                if ("not_www".equals(protocol)) {
+                    url = "http" + "://" + domain;
+                } else {
+                    url = protocol + "://" + mainDomain;
+                }
+
+                long timeout = timeoutSecond > 0 ? timeoutSecond : 3;
+                try (Response response = OkHttpUtils.client(timeout).newCall(OkHttpUtils.request(url)).execute()) {
+                    if (response.isSuccessful() && response.body() != null) {
+
+                        // 编码
+                        byte[] htmlBytes = response.body().bytes();
+                        String charset = CharsetUtils.guessCharset(htmlBytes, response);
+                        String html = new String(htmlBytes, charset);
+
+                        // 信息抽取
+                        Document document = Jsoup.parse(html);
+                        String title = Parse.parseTitle(document);
+                        String cleanTitle = Parse.parseCleanTitle(title);
+                        String keywords = Parse.parseKeywords(document);
+                        String description = Parse.parseDescription(document);
+                        String icp = Parse.parseIcp(html);
+                        Set<String> links = Parse.parseLinks(document, domain, url);
+                        Set<String> subDomains = Parse.parseSubDomain(document, domain, url);
+
+                        return Site.builder()
+                                .protocol(protocol)
+                                .mainDomain(mainDomain)
+                                .charset(charset)
+                                .html(html)
+                                .title(title)
+                                .cleanTitle(cleanTitle)
+                                .keywords(keywords)
+                                .description(description)
+                                .icp(icp)
+                                .subDomain(subDomains)
+                                .links(links)
+                                .build();
+
+                    }
+                } catch (IOException e) {
+                    System.out.println(e.getLocalizedMessage() + ":" + url);
+                    // throw new Exception("site domain except: " + e.getLocalizedMessage() + ":" + url);
                 }
             }
         }
