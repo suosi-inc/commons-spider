f70eace91c3b41bce5f972c48d040ca73dd0bdbe
chengliang <chengl@suosi.com.cn>
2019-12-16 15:52:45 +0800

    * 0.57.12-SNAPSHOT 修改page.url跳转能力

--- src/main/java/com/github/suosi/commons/spider/extract/site/PageExtract.java
+++ src/main/java/com/github/suosi/commons/spider/extract/site/PageExtract.java
@@ -87,6 +87,33 @@ public class PageExtract {
         return null;
     }
 
+    /**
+     * 根据URL，尝试抽取页面信息，带有超时时间
+     *
+     * @param url
+     * @param timeoutSecond
+     * @return
+     */
+    public static Page url(String url, long timeoutSecond, int deep) throws Exception {
+        if (UrlUtils.verifyUrl(url)) {
+            URL parseUrl = UrlUtils.parse(url);
+            if (parseUrl != null) {
+                String host = parseUrl.getHost();
+
+                long timeout = timeoutSecond > 0 ? timeoutSecond : 3;
+                try (Response response = OkHttpUtils.client(timeout).newCall(OkHttpUtils.request(url)).execute()) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        return response(response, host, url, deep);
+                    }
+                } catch (IOException e) {
+                    // System.out.println(e.getLocalizedMessage() + ":" + url);
+                    throw new Exception("page url except: " +e.getLocalizedMessage() + ":" + url);
+                }
+            }
+        }
+
+        return null;
+    }
 
     public static Page response(Response response, String host, String url) throws IOException {
         // 编码
@@ -100,47 +127,69 @@ public class PageExtract {
         String description = Parse.parseDescription(document);
         Set<String> links = Parse.parseLinks(document, host, url);
 
-        // if (links.size() == 0) {
-        //     String refreshUrl = "";
-        //     Matcher matcher1 = LOCATION_PATTERN.matcher(html);
-        //     Matcher matcher2 = LOCATION2_PATTERN.matcher(html);
-        //     Matcher matcher3 = REFRESH_PATTERN.matcher(html);
-        //     Matcher matcher4 = LOCATION3_PATTERN.matcher(html);
-        //     if (matcher1.find()) {
-        //         refreshUrl = matcher1.group(1);
-        //     } else if (matcher2.find()) {
-        //         refreshUrl = matcher2.group(1);
-        //     } else if (matcher3.find()) {
-        //         refreshUrl = matcher3.group(1);
-        //     } else if (matcher4.find()) {
-        //         refreshUrl = matcher4.group(1);
-        //     }
-        //
-        //     if (refreshUrl.length() > 0) {
-        //         try {
-        //             URL absoluteUrl = new URL(url);
-        //
-        //             // path 为空的情况，这种一般是错误，直接移除
-        //             if (StringUtils.isBlank(absoluteUrl.getPath())) {
-        //                 refreshUrl = Parse.removeStartComplete(refreshUrl, "./");
-        //                 refreshUrl = Parse.removeStartComplete(refreshUrl, "../");
-        //             }
-        //
-        //             URL parseUrl = new URL(absoluteUrl, refreshUrl);
-        //             refreshUrl = parseUrl.toString();
-        //
-        //             try (Response response2 = OkHttpUtils.client().newCall(OkHttpUtils.request(refreshUrl)).execute()) {
-        //                 if (response2.isSuccessful() && response2.body() != null) {
-        //                     return response(response2, host, refreshUrl);
-        //                 }
-        //             } catch (IOException e) {
-        //                 System.out.println("refreshurl:" + e.getLocalizedMessage() + ":" + url);
-        //             }
-        //         } catch (IOException e) {
-        //             System.out.println("again error:" + e.getLocalizedMessage() + ":" + refreshUrl);
-        //         }
-        //     }
-        // }
+        return Page.builder()
+                .charset(charset)
+                .html(html)
+                .title(title)
+                .keywords(keywords)
+                .description(description)
+                .links(links)
+                .build();
+    }
+
+    public static Page response(Response response, String host, String url, int deep) throws IOException {
+        // 编码
+        byte[] htmlBytes = response.body().bytes();
+        String charset = CharsetUtils.guessCharset(htmlBytes, response);
+        String html = new String(htmlBytes, charset);
+
+        Document document = Jsoup.parse(html);
+        String title = Parse.parseTitle(document);
+        String keywords = Parse.parseKeywords(document);
+        String description = Parse.parseDescription(document);
+        Set<String> links = Parse.parseLinks(document, host, url);
+
+        if (links.size() == 0 && deep == 0) {
+            String refreshUrl = "";
+            Matcher matcher1 = LOCATION_PATTERN.matcher(html);
+            Matcher matcher2 = LOCATION2_PATTERN.matcher(html);
+            Matcher matcher3 = REFRESH_PATTERN.matcher(html);
+            Matcher matcher4 = LOCATION3_PATTERN.matcher(html);
+            if (matcher1.find()) {
+                refreshUrl = matcher1.group(1);
+            } else if (matcher2.find()) {
+                refreshUrl = matcher2.group(1);
+            } else if (matcher3.find()) {
+                refreshUrl = matcher3.group(1);
+            } else if (matcher4.find()) {
+                refreshUrl = matcher4.group(1);
+            }
+
+            if (refreshUrl.length() > 0) {
+                try {
+                    URL absoluteUrl = new URL(url);
+
+                    // path 为空的情况，这种一般是错误，直接移除
+                    if (StringUtils.isBlank(absoluteUrl.getPath())) {
+                        refreshUrl = Parse.removeStartComplete(refreshUrl, "./");
+                        refreshUrl = Parse.removeStartComplete(refreshUrl, "../");
+                    }
+
+                    URL parseUrl = new URL(absoluteUrl, refreshUrl);
+                    refreshUrl = parseUrl.toString();
+
+                    try (Response response2 = OkHttpUtils.client().newCall(OkHttpUtils.request(refreshUrl)).execute()) {
+                        if (response2.isSuccessful() && response2.body() != null) {
+                            return response(response2, host, refreshUrl, deep + 1);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("refreshurl:" + e.getLocalizedMessage() + ":" + url);
+                    }
+                } catch (IOException e) {
+                    System.out.println("again error:" + e.getLocalizedMessage() + ":" + refreshUrl);
+                }
+            }
+        }
         return Page.builder()
                 .charset(charset)
                 .html(html)
